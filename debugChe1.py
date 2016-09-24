3a95547f6c1a63cfe538583960ac836490dd20a7
Che B <Che@Che-no-MacBook-Air.local>
2016-09-23 20:58:55 -0400

    first commit

---
+++ debugChe1.py
@@ -0,0 +1,22 @@
+bill_total = int(raw_input('What was the total bill?' ))
+service_rating = raw_input('Rate the service (Good, Fair, or Bad): ')
+
+
+if service_rating == 'Good':
+    tip_total = bill_total * 0.20
+if service_rating == 'Fair':
+    tip_total = bill_total * 0.15
+if service_rating == 'Bad':
+    tip_total = bill_total * 0.10
+
+print 'Please Type Good, Fair, or Bad as a rating.'
+
+total = bill_total + tip_total
+
+print total
+
+split_ways = int(raw_input('Split the bill how many ways? (1 for none): '))
+
+each_person = total / split_ways
+
+print 'Amount per person ''%.2f' % each_person
---
+++ printsqr.py
@@ -0,0 +1,4 @@
+how_big = int(raw_input('How big is the square?'))
+
+for number in range(how_big)
+    print '*'
